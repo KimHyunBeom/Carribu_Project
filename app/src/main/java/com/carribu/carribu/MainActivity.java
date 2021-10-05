605051487c527145ea57888ab00b53471e732c4f
rse444 <rse444@naver.com>
2021-10-05 15:19:03 +0900

    listview 가벼운 기초 설정

--- app/src/main/java/com/carribu/carribu/MainActivity.java
+++ app/src/main/java/com/carribu/carribu/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_layout);
+        setContentView(R.layout.activity_main);
         setTitle("동서울대 맛집 리스트");
 
         final String[] mid = { "중식", "일식", "햄버거", "횟집" , "돈까스" ,  "면", "떡볶이", "카페", "한식",  "양식"
