9cee2dbbf7ea8e35525b175c9e08cc780cb4107e
rse444 <rse444@naver.com>
2021-10-05 15:16:37 +0900

    listview 가벼운 기초 설정

--- app/src/main/java/com/carribu/carribu/MainActivity.java
+++ app/src/main/java/com/carribu/carribu/MainActivity.java
@@ -3,12 +3,35 @@ package com.carribu.carribu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_layout);
+        setTitle("동서울대 맛집 리스트");
+
+        final String[] mid = { "중식", "일식", "햄버거", "횟집" , "돈까스" ,  "면", "떡볶이", "카페", "한식",  "양식"
+                , "술집", "치킨집", "기타" };
+
+        ListView list = (ListView) findViewById(R.id.listView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, mid);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Toast.makeText(getApplicationContext(), mid[arg2],
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
